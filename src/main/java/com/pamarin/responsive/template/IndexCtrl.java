e5936d5578d9fa3039490e9762a630ae77a83201
jittagornp <jittagornp@gmail.com>
2018-04-10 16:51:21 +0700

    add /account

--- src/main/java/com/pamarin/responsive/template/IndexCtrl.java
+++ src/main/java/com/pamarin/responsive/template/IndexCtrl.java
@@ -5,6 +5,7 @@ package com.pamarin.responsive.template;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -14,8 +15,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexCtrl {
 
     @GetMapping({"", "/"})
-    public ModelAndView getIndex() {
-        return new ModelAndView("index");
+    public ModelAndView getIndex(@RequestParam(value = "page", required = false, defaultValue = "index") String page) {
+        ModelAndView mav = new ModelAndView("template");
+        mav.addObject("page", page);
+        return mav;
     }
 
 }
